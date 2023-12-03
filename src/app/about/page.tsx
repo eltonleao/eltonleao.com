6bac67477c221213e0bf4965bbf67423852aaa99
eltonleao <3lton.leao@gmail.com>
2023-12-03 14:19:40 -0300

    Add about page components and sections

---
+++ src/app/about/page.tsx
@@ -0,0 +1,19 @@
+// components
+import { Navbar, Footer } from "@/components";
+
+// sections
+import Hero from "../hero";
+import LatestBlogPosts from "../latest-blog-posts";
+import ContactForm from "../contact-form";
+
+export default function Campaign() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <LatestBlogPosts />
+      <ContactForm />
+      <Footer />
+    </>
+  );
+}
