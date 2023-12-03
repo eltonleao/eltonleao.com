a7c9b483f178072de3126c038baad3bb01ed07d5
eltonleao <3lton.leao@gmail.com>
2023-12-03 19:21:24 -0300

    Update about page layout

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,16 +2,17 @@
 import { Navbar, Footer } from "@/components";
 
 // sections
-import Hero from "../hero";
-import LatestBlogPosts from "../latest-blog-posts";
 import ContactForm from "../contact-form";
 
 export default function Campaign() {
   return (
     <>
-      <Navbar />
-      <Hero />
-      <LatestBlogPosts />
+      <Navbar isSolid={true} />
+      <section className="py-40 px-8">
+        <div className="container mx-auto mb-12">
+          <h1>about</h1>
+        </div>
+      </section>
       <ContactForm />
       <Footer />
     </>
